50901e5d6a700b351655fecec2ab283423cdb160
Chernikov Nikita <14nik20@gmail.com>
2025-11-08 23:51:53 +0200

    Added basic Holiday model.

---
+++ src/AllHands.Backend/AllHands.Domain/Models/Holiday.cs
@@ -0,0 +1,9 @@
+﻿namespace AllHands.Domain.Models;
+
+public sealed class Holiday
+{
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required Guid CompanyId { get; set; }
+    public required DateOnly Date { get; set; }
+}
