b13c39fb144254671f273e0155346fd18e0161f7
Chernikov Nikita <14nik20@gmail.com>
2025-11-13 13:32:55 +0200

    Added CompanyId to IdentityUser.

--- src/AllHands.Backend/AllHands.Infrastructure/Auth/Entities/AllHandsIdentityUser.cs
+++ src/AllHands.Backend/AllHands.Infrastructure/Auth/Entities/AllHandsIdentityUser.cs
@@ -4,6 +4,7 @@ namespace AllHands.Infrastructure.Auth.Entities;
 
 public sealed class AllHandsIdentityUser : IdentityUser<Guid>
 {
+    public required Guid CompanyId { get; set; }
     public IList<Invitation> IssuedInvitations { get; set; } = [];
     public IList<Invitation> Invitations { get; set; } = [];
 }
