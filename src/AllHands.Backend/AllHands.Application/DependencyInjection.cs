c4ae339e8087a441a3f7dbecb09057f0ed85f007
Chernikov Nikita <14nik20@gmail.com>
2025-11-27 01:57:09 +0200

    Added FluentValidation and FluentValidation behaviors.

--- src/AllHands.Backend/AllHands.Application/DependencyInjection.cs
+++ src/AllHands.Backend/AllHands.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using AllHands.Application.Behaviors;
+using FluentValidation;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AllHands.Application;
@@ -7,9 +9,11 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddApplication(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddValidatorsFromAssembly(typeof(IApplicationMarker).Assembly);
         services.AddMediatR(opt =>
         {
-            opt.RegisterServicesFromAssemblyContaining<IApplicationMarker>();
+            opt.RegisterServicesFromAssemblyContaining<IApplicationMarker>()
+                .AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
         
         return services.AddSingleton(TimeProvider.System);
---
+++ src/AllHands.Backend/AllHands.Application/Utilities/FluentValidationUtilities.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation.Results;
+
+namespace AllHands.Application.Utilities;
+
+public static class FluentValidationUtilities
+{
+    public static string FluentValidationFailuresToString(IEnumerable<ValidationFailure> failures)
+    {
+        return string.Join(Environment.NewLine, failures.Select(f => f.ErrorMessage));
+    }
+}
