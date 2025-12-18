03bc17a320dea9a24bf6b5534645b7149c2dba0a
Chernikov Nikita <14nik20@gmail.com>
2025-12-18 11:12:21 +0200

    Added Position topic.

--- src/AllHands.Shared/AllHands.Shared.Contracts/Messaging/Topics.cs
+++ src/AllHands.Shared/AllHands.Shared.Contracts/Messaging/Topics.cs
@@ -2,7 +2,8 @@
 
 public static class Topics
 {
-    public const string News = $"{Services.NewsService}_news-events";
-    public const string Employee = $"{Services.EmployeeService}_employee-events";
-    public const string Company = $"{Services.EmployeeService}_company-events";
+    public const string News = "news-events";
+    public const string Employee = "employee-events";
+    public const string Company = "company-events";
+    public const string Position = "position-events";
 }
