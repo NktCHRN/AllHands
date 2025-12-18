0ef0eb745a8a0bb328c200dbee46a47def340d4c
Chernikov Nikita <14nik20@gmail.com>
2025-12-18 11:02:33 +0200

    Updated topics.

--- src/AllHands.Shared/AllHands.Shared.Contracts/Messaging/Topics.cs
+++ src/AllHands.Shared/AllHands.Shared.Contracts/Messaging/Topics.cs
@@ -2,7 +2,7 @@
 
 public static class Topics
 {
-    public const string NewsPost = $"{Services.NewsService}_news-post-events";
+    public const string News = $"{Services.NewsService}_news-events";
     public const string Employee = $"{Services.EmployeeService}_employee-events";
     public const string Company = $"{Services.EmployeeService}_company-events";
 }
