65906f3c2deab783eeebb79e29594921d6725d01
Chernikov Nikita <14nik20@gmail.com>
2025-12-19 13:14:16 +0200

    Updated NuGet packages.

--- src/AllHands.Shared/AllHands.Shared.Contracts/Messaging/Events/Users/UserCreatedEvent.cs
+++ src/AllHands.Shared/AllHands.Shared.Contracts/Messaging/Events/Users/UserCreatedEvent.cs
@@ -1,6 +1,6 @@
 ﻿namespace AllHands.Shared.Contracts.Messaging.Events.Users;
 
-public sealed record UserCreatedEvent(Guid Id, IReadOnlyList<Guid> RoleIds, Guid CompanyId) : IAllHandsEvent
+public sealed record UserCreatedEvent(Guid Id, Guid GlobalUserId, IReadOnlyList<Guid> RoleIds, Guid CompanyId) : IAllHandsEvent
 {
     public string GroupId => Id.ToString();
     public DateTimeOffset OccurredAt { get; } = DateTimeOffset.UtcNow;
