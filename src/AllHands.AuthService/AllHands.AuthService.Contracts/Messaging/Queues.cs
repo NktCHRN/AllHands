8a6a2c89fe0850693de8c70d6674c7098152e7b3
Chernikov Nikita <14nik20@gmail.com>
2025-12-23 09:21:49 +0200

    Removed unused Queues constants.

--- src/AllHands.AuthService/AllHands.AuthService.Contracts/Messaging/Queues.cs
+++ src/AllHands.AuthService/AllHands.AuthService.Contracts/Messaging/Queues.cs
@@ -2,8 +2,6 @@
 
 public static class Queues
 {
-    public const string CompanySessionsRecalculationRequestedEvent = "company-sessions-recalculation-requested-event";
-    public const string UserSessionsRecalculationRequestedEvent = "user-sessions-recalculation-requested-event";
     public const string UserInvitedEvent = "user-invited-event";
     public const string ResetPasswordRequestedEvent = "reset-password-requested-event";
 }
